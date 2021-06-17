b47be7af049d131aca98d8da5709e1a7c3947e77
Francoveliz <francoleandroveliz@gmail.com>
2021-06-16 23:59:17 -0300

    go to details, button functionality

--- components/list-item/ListItem.js
+++ components/list-item/ListItem.js
@@ -1,12 +1,15 @@
 import React from "react";
+import Link from "next/link";
 
 const ListItem = ({ id, title }) => {
 	return (
 		<div className="d-flex justify-content-between align-items-center shadow-sm py-2 px-4">
 			<p className="flex-grow-1 m-0">{title}</p>
-			<button className="btn btn-outline-primary ms-3">
-				<i className="bi bi-eye" />
-			</button>
+			<Link href={`posts/${id}`}>
+				<button className="btn btn-outline-primary ms-3">
+					<i className="bi bi-eye" />
+				</button>
+			</Link>
 			<button className="btn btn-outline-primary ms-3">
 				<i className="bi bi-pencil" />
 			</button>
