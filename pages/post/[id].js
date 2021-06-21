28884e590df962337d47337697fd4c5aa7fb94ab
Francoveliz <francoleandroveliz@gmail.com>
2021-06-21 01:50:29 -0300

    details page style

--- pages/post/[id].js
+++ pages/post/[id].js
@@ -5,10 +5,25 @@ const Details = ({ post }) => {
 		<>
 			{post.title ? (
 				<div>
-					<p>{`Title: ${post.title}`}</p>
-					<p>{`Body: ${post.body}`}</p>
-					<p>{`User id: ${post.userId}`}</p>
-					<p>{`Post id: ${post.id}`}</p>
+					<p>
+						<span className="fw-bold text-primary">Title: </span>
+						<span>{post.title}</span>
+					</p>
+
+					<p>
+						<span className="fw-bold text-primary">Body: </span>
+						<span>{post.body}</span>
+					</p>
+
+					<p>
+						<span className="fw-bold text-primary">User Id: </span>
+						<span>{post.userId}</span>
+					</p>
+
+					<p>
+						<span className="fw-bold text-primary">Id: </span>
+						<span>{post.id}</span>
+					</p>
 				</div>
 			) : (
 				<>
