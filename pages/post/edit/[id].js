85dae3df31522aefa93592fbef521e1c75837585
Francoveliz <francoleandroveliz@gmail.com>
2021-06-21 02:29:55 -0300

    edit post validation

--- pages/post/edit/[id].js
+++ pages/post/edit/[id].js
@@ -6,51 +6,77 @@ const EditPost = ({ post }) => {
 	const { id, userId } = post;
 
 	const initialValues = { title: post.title, body: post.body };
+
+	const validate = values => {
+		const errors = {};
+
+		if (!values.title) {
+			errors.title = "Campo obligatorio";
+		}
+
+		if (!values.body) {
+			errors.body = "Campo obligatorio";
+		}
+
+		return errors;
+	};
+
+	const handleSubmit = async values => {
+		try {
+			await updatePost({
+				id,
+				userId,
+				title: values.title,
+				body: values.body,
+			});
+			alert("post actualizado con exito");
+		} catch (error) {
+			throw new Error(error);
+		}
+	};
+
 	return (
 		<>
 			{post.title ? (
 				<Formik
 					initialValues={initialValues}
-					onSubmit={async values => {
-						try {
-							await updatePost({
-								id,
-								userId,
-								title: values.title,
-								body: values.body,
-							});
-							alert("post actualizado con exito");
-						} catch (error) {
-							throw new Error(error);
-						}
-					}}>
-					<Form>
-						<div className="mb-3">
-							<label
-								htmlFor="title"
-								className="form-label text-primary fw-bold">
-								Title
-							</label>
-							<Field id="title" name="title" className="form-control" />
-						</div>
-						<div className="mb-3 h-50">
-							<label
-								htmlFor="body"
-								className="form-label text-primary fw-bold">
-								Body
-							</label>
-							<Field
-								as="textarea"
-								id="body"
-								name="body"
-								className="form-control "
-								style={{ height: "20rem" }}
-							/>
-						</div>
-						<button type="submit" className="btn btn-primary">
-							Submit
-						</button>
-					</Form>
+					onSubmit={values => handleSubmit(values)}
+					validate={values => validate(values)}>
+					{({ errors }) => (
+						<Form>
+							<div className="mb-3">
+								<label
+									htmlFor="title"
+									className="form-label text-primary fw-bold">
+									Title
+								</label>
+								<Field id="title" name="title" className="form-control" />
+							</div>
+							{errors.title && (
+								<p className="form-text text-danger">{errors.title}</p>
+							)}
+							<div className="mb-3 h-50">
+								<label
+									htmlFor="body"
+									className="form-label text-primary fw-bold">
+									Body
+								</label>
+								<Field
+									as="textarea"
+									id="body"
+									name="body"
+									className="form-control "
+									style={{ height: "20rem" }}
+								/>
+								{errors.body && (
+									<p className="form-text text-danger">{errors.body}</p>
+								)}
+							</div>
+							<button type="submit" className="btn btn-primary">
+								Submit
+							</button>
+						</Form>
+					)}
 				</Formik>
 			) : (
 				<h1>Post no encontrado </h1>
