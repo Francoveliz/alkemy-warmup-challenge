edab9fba217d1b857dac2c90a911eadc80b1dfaa
Francoveliz <francoleandroveliz@gmail.com>
2021-06-21 02:26:54 -0300

    post edit validation

--- pages/post/edit/[id].js
+++ pages/post/edit/[id].js
@@ -8,49 +8,53 @@ const EditPost = ({ post }) => {
 	const initialValues = { title: post.title, body: post.body };
 	return (
 		<>
-			<Formik
-				initialValues={initialValues}
-				onSubmit={async values => {
-					try {
-						await updatePost({
-							id,
-							userId,
-							title: values.title,
-							body: values.body,
-						});
-						alert("post actualizado con exito");
-					} catch (error) {
-						throw new Error(error);
-					}
-				}}>
-				<Form>
-					<div className="mb-3">
-						<label
-							htmlFor="title"
-							className="form-label text-primary fw-bold">
-							Title
-						</label>
-						<Field id="title" name="title" className="form-control" />
-					</div>
-					<div className="mb-3 h-50">
-						<label
-							htmlFor="body"
-							className="form-label text-primary fw-bold">
-							Body
-						</label>
-						<Field
-							as="textarea"
-							id="body"
-							name="body"
-							className="form-control "
-							style={{ height: "20rem" }}
-						/>
-					</div>
-					<button type="submit" className="btn btn-primary">
-						Submit
-					</button>
-				</Form>
-			</Formik>
+			{post.title ? (
+				<Formik
+					initialValues={initialValues}
+					onSubmit={async values => {
+						try {
+							await updatePost({
+								id,
+								userId,
+								title: values.title,
+								body: values.body,
+							});
+							alert("post actualizado con exito");
+						} catch (error) {
+							throw new Error(error);
+						}
+					}}>
+					<Form>
+						<div className="mb-3">
+							<label
+								htmlFor="title"
+								className="form-label text-primary fw-bold">
+								Title
+							</label>
+							<Field id="title" name="title" className="form-control" />
+						</div>
+						<div className="mb-3 h-50">
+							<label
+								htmlFor="body"
+								className="form-label text-primary fw-bold">
+								Body
+							</label>
+							<Field
+								as="textarea"
+								id="body"
+								name="body"
+								className="form-control "
+								style={{ height: "20rem" }}
+							/>
+						</div>
+						<button type="submit" className="btn btn-primary">
+							Submit
+						</button>
+					</Form>
+				</Formik>
+			) : (
+				<h1>Post no encontrado </h1>
+			)}
 		</>
 	);
 };
